413fb994387e56fb5e069a0e761ba13c18d2194a
Worthy <worthyintech@gmail.com>
2024-05-28 12:45:31 +0100

    Written the API functionality.

---
+++ main.js
@@ -0,0 +1,25 @@
+const url = "https://api.github.com/userkamranahmedse";
+
+fetch(url).then(
+  function(request) {
+    if (request.status !== 200) {
+      console.log(`Looks like there was a problem.\nStatus Code: ${request.status}`);
+    }
+
+    request.json().then(
+      function(data) {
+        console.log(data);
+      }
+    )
+
+    return;
+  }
+).catch(
+  function(err) {
+    console.log(`The following error was found: ${err}`);
+  }
+);
+
+function buildProfile(profileData) {
+  
+}
